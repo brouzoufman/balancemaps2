1f42aac6a9ba1e0769740ca67007c4ff5adde797
ijon <segfaultsamurai@gmail.com>
2017-05-20 04:03:20 -0500

    Separated point ID from point order
    This allows a map to have multiple points with the same order value,
    which might be useful in some circumstances.
    
    Note that the core does nothing with this order value - it's for the
    map to use.

--- pk3/acs/returnPoints.h
+++ pk3/acs/returnPoints.h
@@ -1,41 +1,53 @@
 #define BRETURN_MAXPOINTS 128
 
 int BReturn_PlayerPoints[PLAYERMAX];
-int BReturn_PointTIDs[BRETURN_MAXPOINTS];
+int BReturn_PointData[BRETURN_MAXPOINTS][2];
+int BReturn_PointCount = 0;
 
-function int BReturn_RegisterPoint(int pointID, int pointTID)
+function int BReturn_Register(int pointOrder)
 {
-	if(pointID < 0 || pointID >= BRETURN_MAXPOINTS) return 0;
-	
-	BReturn_PointTIDs[pointID] = pointTID;
-	return 1;
+    int myTID   = defaultTID(-1);
+    int myIndex = BReturn_PointCount;
+    
+    if (myIndex >= BRETURN_MAXPOINTS)
+    {
+        Log(s:"\cgERROR:\ca Ran out of slots to register return points in (max: ", d:BRETURN_MAXPOINTS, s:")");
+        return 0;
+    }
+    
+    BReturn_PointData[myIndex][0] = myTID;
+    BReturn_PointData[myIndex][1] = pointOrder;
+    BReturn_PointCount++;
+    
+    Log(s:"\cqDEBUG:\cd Registered return point ", d:myIndex, s:" (TID: ", d: myTID, s:", order: ", d:pointOrder, s:")");
+    return 1;
 }
 
 // reset player Point IDs to -1
 function void BReturn_ResetPlayerPoints(void)
 {
-	for(int i = 0; i < PLAYERMAX; i++) BReturn_PlayerPoints[i] = -1;
+    for(int i = 0; i < PLAYERMAX; i++) BReturn_PlayerPoints[i] = -1;
 }
 
 function int BReturn_GetPlayerPoint(int pln)
 {
-	if(pln < 0 || pln >= PLAYERMAX) return -1;
-	return BReturn_PlayerPoints[pln];
+    if(pln < 0 || pln >= PLAYERMAX) return -1;
+    return BReturn_PlayerPoints[pln];
 }
 
 function int BReturn_GetPlayerPointTID(int pln)
 {
-	if(pln < 0 || pln >= PLAYERMAX) return -1;
-	return BReturn_PointTIDs[BReturn_PlayerPoints[pln]];
+    if(pln < 0 || pln >= PLAYERMAX) return -1;
+    return BReturn_PointData[BReturn_PlayerPoints[pln]][0];
 }
 
 function int BReturn_SetPlayerPoint(int pln, int pointID)
 {
-	if(pln < 0 || pln >= PLAYERMAX) return 0;
-	if(pointID < 0 || pointID >= BRETURN_MAXPOINTS) return 0;
-	
-	BReturn_PlayerPoints[pln] = pointID;
-	return 1;
+    if(pln < 0 || pln >= PLAYERMAX) return 0;
+    if(pointID < 0 || pointID >= BReturn_PointCount) return 0;
+    
+    BReturn_PlayerPoints[pln] = pointID;
+    return 1;
 }
 
 
@@ -48,38 +60,44 @@ script "BReturn_TeleportPointHook" (int pointIn)
 
 function int BReturn_TeleportToPoint(int tid, int pointID, int fog)
 {
-	if(pointID < 0 || pointID >= BRETURN_MAXPOINTS) return 0;
-    
     int hookedID = ACS_NamedExecuteWithResult("BReturn_TeleportPointHook", pointID);
-	int pointTID = BReturn_PointTIDs[hookedID];
+    
+    if (hookedID < 0) { return 0; }
+    if (hookedID >= BReturn_PointCount)
+    {
+        Log(s:"\ckWARNING:\cf Tried to teleport to invalid point ID ", d:hookedID, s:" (", d:pointID, s:" before hook)");
+        return 0;
+    }
+    
+    int pointTID = BReturn_PointData[hookedID][0];
 
-	return TeleportFunctional(tid, pointTID, fog, 0);
+    return TeleportFunctional(tid, pointTID, fog, 0);
 }
 
 
 
-script "BReturn_RegisterPoint" (int pointID, int pointTID)
+script "BReturn_Register" (int pointOrder)
 {
-	SetResultValue(BReturn_RegisterPoint(pointID, pointTID));
+    SetResultValue(BReturn_Register(pointOrder));
 }
 
 script "BReturn_GetPlayerPoint" (int pln)
 {
-	SetResultValue(BReturn_GetPlayerPoint(pln));
+    SetResultValue(BReturn_GetPlayerPoint(pln));
 }
 
 script "BReturn_GetPlayerPointTID" (int pln)
 {
-	SetResultValue(BReturn_GetPlayerPointTID(pln));
+    SetResultValue(BReturn_GetPlayerPointTID(pln));
 }
 
 script "BReturn_SetPlayerPoint" (int pln, int pointID)
 {
-	SetResultValue(BReturn_SetPlayerPoint(pln, pointID));
-	Print(s:"\cqDEBUG:\cd Set player ", d:pln, s:"'s point to ", d:pointID);
+    SetResultValue(BReturn_SetPlayerPoint(pln, pointID));
+    Print(s:"\cqDEBUG:\cd Set player ", d:pln, s:"'s point to ", d:pointID);
 }
 
 script "BReturn_TeleportToPoint" (int tid, int pointID, int fog)
 {
-	SetResultValue(BReturn_TeleportToPoint(tid, pointID, fog));
+    SetResultValue(BReturn_TeleportToPoint(tid, pointID, fog));
 }
